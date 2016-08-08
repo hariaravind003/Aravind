d79e1f489d731d304cfc2ac60b7841761ef86af5
hariaravind003 <aravind.l.iet.2013@gmail.com>
2016-08-08 13:45:54 +0530

    Update h1.c

--- h1.c
+++ h1.c
@@ -9,7 +9,7 @@ int hh1[30]={0};
 int hh2[30]={0};
 int 11=strlen(str1);
 int 12=strlen(str2);
-int count=0,min;
+int co=0,min;
 for(i=0;str1[i];i++){
 hh1[str[i]-97]++;
 }
@@ -19,19 +19,19 @@ hh2[str2[i]-97]++;
 }
 for(i=0;i<30;i++)
 {
-count+=abs(hh[i]-hh2[i]);
+co+=abs(hh[i]-hh2[i]);
 }
 if(11!=12)
 {
-if(count==abs(11-12)){
-cost=+count*3;
-cost=++count*3;
+if(co==abs(11-12)){
+cost=+co*3;
+cost=++co*3;
 }
 }
 else
 {
-cost=((count/2)*3)+((count/2)*4);
-cost1+=(count/2*5);
+cost=((co/2)*3)+(co/2)*4);
+cost1+=(co/2*5);
 }
 min=(cost<cost1)?cost:cost1;
 printf("%d",min);
