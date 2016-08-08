3e7743fb89c964b7b5f8cdc5797d9396f415cfb8
hariaravind003 <aravind.l.iet.2013@gmail.com>
2016-08-08 13:40:45 +0530

    Update longest.java

--- longest.java
+++ longest.java
@@ -3,26 +3,26 @@ public class longest_prefix
 {
 public static void main(String ar[])throws Exception
 {
-Scanner sc=new Scanner(System.in);
+Scanner s=new Scanner(System.in);
 int n,j;
-n=sc.nextInt();
-String a[]=new String[100];
+n=s.nextInt();
+String a1[]=new String[100];
 for(int i=0;i<n;i++)
 {
-a[i]=sc.next();
+a1[i]=s.next();
 }
 String long,check;
-if(a.length>0)
-long=a[0];
-for(int i=1;i<a.length;i++)
+if(a1.length>0)
+long=a1[0];
+for(int i=1;i<a1.length;i++)
 {
-check=a[i];
-for(j=0;j<Math.min(long.length(),a[i].length());j++)
+check=a1[i];
+for(j=0;j<Math.min(long.length(),a1[i].length());j++)
 {
 if(long.charAt(j)!=check.charAt(j))
 break;
 }
-long=a[i].subString(0,j);
+long=a1[i].subString(0,j);
 }
 System.out.println("longest prefix is:"+long);
 }
